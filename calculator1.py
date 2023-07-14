2a2f747efa45b6307e22ef64bb1eb4cb9b8c142c
rahul <rahulchowdhary.j@gmail.com>
2023-07-14 13:37:46 +0530

    first commit

---
+++ calculator1.py
@@ -0,0 +1,5 @@
+import calculator1
+a=int(input('enter:'))
+b=int(input('enter:'))
+print(calculator1.add(a,b))
+print(calculator1.sub(a,b))
---
+++ modules.py
@@ -0,0 +1,5 @@
+import calculator
+a=int(input('enter a number:'))
+b=int(input('enter a number:'))
+print(calculator.add(a,b))
+print(calculator.sub(a,b))
